da5ea96fffd3ce580d4be4972c00d4c3e7608513
Sebastian Kerkmann <basti.kerkmann@freenet.de>
2026-08-18 16:53:17 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::SplitWhitespace;
 
 fn main() {
     loop {
@@ -8,10 +9,19 @@ fn main() {
         let mut user_input = String::new();
         io::stdin().read_line(&mut user_input).unwrap();
         user_input = user_input.trim().to_string();
+        let mut user_input_iter = user_input.split_whitespace();
 
-        match user_input.as_str() {
-            "exit" => std::process::exit(0),
+        match user_input_iter.next() {
+            Some("exit") => std::process::exit(0),
+            Some("echo") => print_slices(user_input_iter),
             _ => println!("{user_input}: command not found"),
         }
     }
+}
+
+fn print_slices(str_iter: SplitWhitespace<'_>) {
+    for ele in str_iter {
+        print!("{} ", ele)
+    }
+    println!()
 }
