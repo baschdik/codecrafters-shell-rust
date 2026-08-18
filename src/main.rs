f6065314e5bdd96c28f5fbb6cb30fa6c4d189e09
Sebastian Kerkmann <basti.kerkmann@freenet.de>
2026-08-18 16:00:05 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,7 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).unwrap();
+    print!("{user_input}: command not found")
 }
