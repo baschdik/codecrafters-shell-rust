eefa6f1bcdaa6f04985e80b0aa0068bc1fcb5f95
Sebastian Kerkmann <basti.kerkmann@freenet.de>
2026-08-18 16:23:38 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
         let mut user_input = String::new();
         io::stdin().read_line(&mut user_input).unwrap();
         user_input = user_input.trim().to_string();
-        println!("{user_input}: command not found")
+
+        match user_input.as_str() {
+            "exit" => std::process::exit(0),
+            _ => println!("{user_input}: command not found"),
+        }
     }
 }
