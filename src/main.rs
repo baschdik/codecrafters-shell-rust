e5b1474d07bf8bb5e4f3e5965c124b20a1a9bdea
Sebastian Kerkmann <basti.kerkmann@freenet.de>
2026-08-20 14:46:08 +0200

    Refactored: Builtin are now in an HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::str::SplitWhitespace;
+use std::{collections::HashMap, str::SplitWhitespace};
 
 fn main() {
     loop {
@@ -11,10 +11,28 @@ fn main() {
         user_input = user_input.trim().to_string();
         let mut user_input_iter = user_input.split_whitespace();
 
-        match user_input_iter.next() {
-            Some("exit") => std::process::exit(0),
-            Some("echo") => builtin_echo(user_input_iter),
-            _ => println!("{user_input}: command not found"),
+        let builtin_commands: HashMap<&str, fn(SplitWhitespace<'_>)> = HashMap::from([
+            ("echo", builtin_echo as fn(SplitWhitespace<'_>)),
+            ("exit", builtin_exit),
+            ("type", builtin_type),
+        ]);
+
+        let user_input_1 = user_input_iter.next().expect("No Some here");
+
+        if user_input_1 == "type" {
+            let cmd_to_test = user_input_iter.next().expect("No Some here");
+            if builtin_commands.contains_key(&cmd_to_test) {
+                println!("{} is a shell builtin", cmd_to_test);
+            } else {
+                println!("{}: not found", cmd_to_test);
+            }
+            continue;
+        }
+
+        let current_command = builtin_commands.get(user_input_1);
+        match current_command {
+            Some(func) => func(user_input_iter),
+            None => println!("{user_input}: command not found"),
         }
     }
 }
@@ -25,3 +43,9 @@ fn builtin_echo(str_iter: SplitWhitespace<'_>) {
     }
     println!()
 }
+
+fn builtin_exit(_str_iter: SplitWhitespace<'_>) {
+    std::process::exit(0)
+}
+
+fn builtin_type(_str_iter: SplitWhitespace<'_>) {} // no implementation -> only to be found as builtin
