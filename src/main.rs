1dd181ce6fb04f7a278f53abf66586c3ac92419a
Sebastian Kerkmann <basti.kerkmann@freenet.de>
2026-08-20 20:24:24 +0200

    Implemented Type with  Enum/trait

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,64 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{collections::HashMap, str::SplitWhitespace};
+use std::str::FromStr;
+
+enum Builtins {
+    Echo,
+    Exit,
+    Type,
+}
+
+impl FromStr for Builtins {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "echo" => Ok(Builtins::Echo),
+            "exit" => Ok(Builtins::Exit),
+            "type" => Ok(Builtins::Type),
+            _ => Err(()),
+        }
+    }
+}
 
 fn main() {
-    let builtin_commands: HashMap<&str, fn(SplitWhitespace<'_>)> = HashMap::from([
-        ("echo", builtin_echo as fn(SplitWhitespace<'_>)),
-        ("exit", builtin_exit),
-        ("type", builtin_type),
-    ]);
-
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut user_input = String::new();
         io::stdin().read_line(&mut user_input).unwrap();
         user_input = user_input.trim().to_string();
-        let mut user_input_iter = user_input.split_whitespace();
+        let user_input_split: Vec<&str> = user_input.split_whitespace().collect();
 
-        let user_input_1 = user_input_iter.next().expect("No Some here");
-
-        if user_input_1 == "type" {
-            let cmd_to_test = user_input_iter.next().expect("No Some here");
-            if builtin_commands.contains_key(&cmd_to_test) {
-                println!("{} is a shell builtin", cmd_to_test);
-            } else {
-                println!("{}: not found", cmd_to_test);
-            }
+        let command = user_input_split[0].parse::<Builtins>();
+        if command.is_err() {
+            println!("{}: command not found", &user_input_split[0]);
             continue;
         }
 
-        let current_command = builtin_commands.get(user_input_1);
-        match current_command {
-            Some(func) => func(user_input_iter),
-            None => println!("{user_input}: command not found"),
+        match command.unwrap() {
+            Builtins::Echo => builtin_echo(user_input_split),
+            Builtins::Exit => builtin_exit(),
+            Builtins::Type => builtin_type(user_input_split),
         }
     }
 }
 
-fn builtin_echo(str_iter: SplitWhitespace<'_>) {
-    for ele in str_iter {
+fn builtin_echo(str_iter: Vec<&str>) {
+    for ele in &str_iter[1..] {
         print!("{} ", ele)
     }
     println!()
 }
 
-fn builtin_exit(_str_iter: SplitWhitespace<'_>) {
+fn builtin_exit() {
     std::process::exit(0)
 }
 
-fn builtin_type(_str_iter: SplitWhitespace<'_>) {} // no implementation -> only to be found as builtin
+fn builtin_type(str_split: Vec<&str>) {
+    let typed_cmd = str_split[1].parse::<Builtins>();
+    match typed_cmd {
+        Ok(_) => println!("{} is a shell builtin", str_split[1]),
+        Err(_) => println!("{}: not found", str_split[1]),
+    }
+}
