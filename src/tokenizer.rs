766d9bf4b21bf47a4471c31e403f40ecc950770a
Sebastian Kerkmann <basti.kerkmann@freenet.de>
2026-09-19 17:21:05 +0200

    Some updates

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -6,62 +6,59 @@ enum Quotation {
 }
 
 pub fn tokenize(input: &String) -> Vec<String> {
-    let mut splited = vec!["".to_string()];
-    let mut splited_qoutation = vec![Quotation::No];
+    let mut processed = vec!["".to_string()];
+    let mut qoutation_status = Quotation::No;
 
     let mut input_iter = input.chars().peekable();
     while let Some(c) = input_iter.next() {
-        match splited_qoutation.last().unwrap() {
+        match qoutation_status {
             Quotation::No => {
                 if c.is_whitespace() {
-                    splited.push("".to_string());
+                    processed.push("".to_string());
                     //remove extra whitespace:
                     while input_iter.next_if(|c| c.is_whitespace()).is_some() {}
                 } else if c == '\'' {
                     //Ignore Empty quotes '':
-                    if input_iter.peek() == Some(&'\'') {
-                        _ = input_iter.next();
+                    if input_iter.next_if(|c| *c == '\'').is_some() {
                         continue;
                     }
-                    splited_qoutation.push(Quotation::Single);
+
+                    qoutation_status = Quotation::Single;
                 } else if c == '"' {
                     //Ignore Empty quotes "":
-                    if input_iter.peek() == Some(&'"') {
-                        _ = input_iter.next();
+                    if input_iter.next_if(|c| *c == '"').is_some() {
                         continue;
                     }
-                    splited_qoutation.push(Quotation::Double);
+                    qoutation_status = Quotation::Double;
                 } else {
-                    splited.last_mut().unwrap().push(c);
+                    processed.last_mut().unwrap().push(c);
                 }
             }
             Quotation::Single => {
                 if c == '\'' {
                     //Concatenate adjacent quoted strings:
-                    if input_iter.peek() == Some(&'\'') {
-                        _ = input_iter.next();
+                    if input_iter.next_if(|c| *c == '\'').is_some() {
                         continue;
                     }
-                    splited_qoutation.push(Quotation::No);
+                    qoutation_status = Quotation::No;
                 } else {
-                    splited.last_mut().unwrap().push(c);
+                    processed.last_mut().unwrap().push(c);
                 }
             }
             Quotation::Double => {
                 if c == '"' {
                     //Concatenate adjacent quoted strings:
-                    if input_iter.peek() == Some(&'"') {
-                        _ = input_iter.next();
+                    if input_iter.next_if(|c| *c == '\'').is_some() {
                         continue;
                     }
-                    splited_qoutation.push(Quotation::No);
+                    qoutation_status = Quotation::No;
                 } else {
-                    splited.last_mut().unwrap().push(c);
+                    processed.last_mut().unwrap().push(c);
                 }
             }
         }
     }
 
-    println!("Final: {:?}", splited); //DEBUG
-    splited
+    println!("Final: {:?}", processed); //DEBUG
+    processed
 }
